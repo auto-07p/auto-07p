bd433bcba1a597a1189eebcc5d2555b7e9c6e307
Bart Oldeman <bart.oldeman@mcgill.ca>
2006-08-24 21:24:18 +0000

    Updated auto.env and added auto.env.sh for sh/bash/etc users.
    git-svn-id: https://svn.code.sf.net/p/auto-07p/code/trunk/auto/07p@1076 ee9e63dd-6847-0410-88cd-b7174c777ff7

---
+++ cmds/auto.env.sh
@@ -0,0 +1,11 @@
+# 
+#  This file has to be sourced before activating AUTO if you are using
+#  a 'sh' compatible shell, such as sh, bash, ksh, or ash.
+#
+AUTO_DIR=$HOME/auto/07p
+PATH=$AUTO_DIR/cmds:$AUTO_DIR/bin:$PATH
+export AUTO_DIR
+export PATH
+#
+# DON'T ADD ANYTHING AFTER THIS LINE
+#
