2335f9b2afe243a5d2a3ee69b5367698680623ce
Bart Oldeman <bart.oldeman@mcgill.ca>
2007-05-22 19:04:52 +0000

    Merged vNames.h with gVarNames.h
    git-svn-id: https://svn.code.sf.net/p/auto-07p/code/trunk/auto/07p@1198 ee9e63dd-6847-0410-88cd-b7174c777ff7

--- plaut04/includes/gVarNames.h
+++ plaut04/includes/gVarNames.h
@@ -1,3 +1,9 @@
+#ifndef R3B
+-- gVarNames.h	2007-05-09 15:57:05.880290000 -0400
+#else
+++ vNames.h	2007-05-09 15:52:43.519888000 -0400
+@ -1,71 +1,86 @@
+#endif
     char *typeTokenNames[] =
     {
         "DEFAULT","BP ALG", "LP ALG",
@@ -11,19 +17,46 @@
     {
         "Graph Type",
         "Graph Style",
+#ifdef R3B
+        "Coordinate system",
+#endif
         "Window Width",
         "Window Height",
+//        "Labels",
         "Coloring Method",
+#ifdef R3B
+        "Number of Period Animated",
+#endif
         "Line Width Scaler",
+#ifndef R3B
         "Object Radius",
+#else
+        "Satellite Radius",
+        "Large Primary Radius",
+        "Small Primary Radius",
+        "Number of Stars",
+        "Libration Point Size",
+#endif
         "AniLine Thickness Scaler",
+#ifndef R3B
         "Object Max Animation Speed",
         "Object Min Animation Speed",
         "Orbit Max Animation Speed",
         "Orbit Min Animation Speed",
+#else
+        "Sat Max Animation Speed",
+        "Sat Min Animation Speed",
+#endif
         "Coordinate Type",
         "Background Transparency",
+#ifndef R3B
         "Number of Period Animated",
+#else
+	"Disk Transparency",
+	"Disk From File",
+        "Orbit Max Animation Speed",
+        "Orbit Min Animation Speed",
+#endif
     };
 
     char * hexdecimalVarNames[] =
@@ -38,10 +71,23 @@
         "X Axis Color",
         "Y Axis Color",
         "Z Axis Color",
+#ifndef R3B
         "Object Color",
         "Surface Color",
+#else
+        "satellite Color",
+        "large primary Color",
+        "large primary tail Color",
+        "small primary Color",
+        "small primary tail Color",
+        "surface Color",
+#endif
         "Unstable Solution Color",
+#ifndef R3B
         "Stable Solution Color",
+#else
+        "Stable Solution Color"
+#endif
     };
 
     char * blWidgetName[] =
@@ -53,8 +99,17 @@
 
 char * graphWidgetItems[]=
 {
+#ifndef R3B
     "Highlight Orbit",
+#else
+    "Draw Reference Plane", 
+    "Draw Primaries", 
+    "Draw Libration Points",
+#endif
     "Orbit Animation", 
+#ifdef R3B
+    "Satellite Animation",
+#endif
     "Draw Background", 
     "Draw Legend", 
     "Normalize Data"
