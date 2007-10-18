ba7917d537244ec5cadc13034599b785d8d72d3b
Bart Oldeman <bart.oldeman@mcgill.ca>
2007-10-18 08:09:29 +0000

    Modernize the bvp demo.
    git-svn-id: https://svn.code.sf.net/p/auto-07p/code/trunk/auto/07p@1330 ee9e63dd-6847-0410-88cd-b7174c777ff7

---
+++ demos/bvp/bvp.f90
@@ -0,0 +1,67 @@
+!---------------------------------------------------------------------- 
+!---------------------------------------------------------------------- 
+!   bvp :    A nonlinear ODE eigenvalue problem
+!---------------------------------------------------------------------- 
+!---------------------------------------------------------------------- 
+
+SUBROUTINE FUNC(NDIM,U,ICP,PAR,IJAC,F,DFDU,DFDP) 
+!--------- ---- 
+
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM, IJAC, ICP(*)
+  DOUBLE PRECISION, INTENT(IN) :: U(NDIM), PAR(*)
+  DOUBLE PRECISION, INTENT(OUT) :: F(NDIM), DFDU(NDIM,*), DFDP(NDIM,*)
+
+  DOUBLE PRECISION pi
+
+  pi = 4*ATAN(1.d0)
+
+  F(1) = U(2)
+  F(2) = -( PAR(1)*pi )**2 * U(1) + U(1)**2
+
+END SUBROUTINE FUNC
+
+SUBROUTINE STPNT(NDIM,U,PAR,T) 
+!--------- ----- 
+
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM
+  DOUBLE PRECISION, INTENT(IN) :: T
+  DOUBLE PRECISION, INTENT(OUT) :: U(NDIM), PAR(*)
+
+  PAR(1:2) = (/ 0.0d0, 0.0d0 /)
+
+  U = (/ 0.0d0, 0.0d0 /)
+
+END SUBROUTINE STPNT
+
+SUBROUTINE BCND(NDIM,PAR,ICP,NBC,U0,U1,FB,IJAC,DBC) 
+!--------- ---- 
+
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM, ICP(*), NBC, IJAC
+  DOUBLE PRECISION, INTENT(IN) :: PAR(*), U0(NDIM), U1(NDIM)
+  DOUBLE PRECISION, INTENT(OUT) :: FB(NBC), DBC(NBC,*)
+
+  FB = (/ U0(1), U1(1) /)
+
+END SUBROUTINE BCND
+
+SUBROUTINE ICND(NDIM,PAR,ICP,NINT,U,UOLD,UDOT,UPOLD,FI,IJAC,DINT)
+!--------- ----
+
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM, ICP(*), NINT, IJAC
+  DOUBLE PRECISION, INTENT(IN) :: PAR(*)
+  DOUBLE PRECISION, INTENT(IN) :: U(NDIM), UOLD(NDIM), UDOT(NDIM), UPOLD(NDIM)
+  DOUBLE PRECISION, INTENT(OUT) :: FI(NINT), DINT(NINT,*)
+
+  FI = (/ U(1)-PAR(2) /)
+
+END SUBROUTINE ICND
+
+SUBROUTINE FOPT 
+END SUBROUTINE FOPT
+
+SUBROUTINE PVLS
+END SUBROUTINE PVLS
