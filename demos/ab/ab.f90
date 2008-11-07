c6a3b1783fcc50b12186e5a6c85eae0a4de42a1d
Bart Oldeman <bart.oldeman@mcgill.ca>
2008-11-07 15:15:57 +0000

    Update ab demo.
    git-svn-id: https://svn.code.sf.net/p/auto-07p/code/trunk/auto/07p@1775 ee9e63dd-6847-0410-88cd-b7174c777ff7

---
+++ demos/ab/ab.f90
@@ -0,0 +1,88 @@
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+!   ab :            The A --> B reaction 
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+!
+      SUBROUTINE FUNC(NDIM,U,ICP,PAR,IJAC,F,DFDU,DFDP)
+!     ---------- ----
+!
+! Evaluates the algebraic equations or ODE right hand side
+!
+! Input arguments :
+!      NDIM   :   Dimension of the ODE system 
+!      U      :   State variables
+!      ICP    :   Array indicating the free parameter(s)
+!      PAR    :   Equation parameters
+!
+! Values to be returned :
+!      F      :   ODE right hand side values
+!
+! Normally unused Jacobian arguments : IJAC, DFDU, DFDP (see manual)
+!
+      IMPLICIT NONE
+      INTEGER NDIM, IJAC, ICP(*)
+      DOUBLE PRECISION U(NDIM), PAR(*), F(NDIM), DFDU(*), DFDP(*)
+      DOUBLE PRECISION U1,U2,E
+!
+       U1=U(1)
+       U2=U(2)
+!
+       E=EXP(U2)
+!
+       F(1)=-U1 + PAR(1)*(1-U1)*E
+       F(2)=-U2 + PAR(1)*PAR(2)*(1-U1)*E - PAR(3)*U2
+!
+      RETURN
+      END
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+!
+      SUBROUTINE STPNT(NDIM,U,PAR,T)
+!     ---------- -----
+!
+! Input arguments :
+!      NDIM   :   Dimension of the ODE system 
+!
+! Values to be returned :
+!      U      :   A starting solution vector
+!      PAR    :   The corresponding equation-parameter values
+!      T      :	  Not used here
+!
+      IMPLICIT NONE
+      INTEGER NDIM
+      DOUBLE PRECISION U(NDIM), PAR(*), T
+!
+! Initialize the equation parameters
+       PAR(1)=0.
+       PAR(2)=8.
+       PAR(3)=3.
+!
+! Initialize the solution
+       U(1)=0.
+       U(2)=0.
+!
+      RETURN
+      END
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+! The following subroutines are not used here,
+! but they must be supplied as dummy routines
+!
+      SUBROUTINE BCND 
+      RETURN 
+      END 
+! 
+      SUBROUTINE ICND 
+      RETURN 
+      END 
+! 
+      SUBROUTINE FOPT 
+      RETURN 
+      END 
+! 
+      SUBROUTINE PVLS
+      RETURN 
+      END 
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
