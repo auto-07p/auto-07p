3d646696b47431324b7ea31632501ddda5ec9585
Bart Oldeman <bartoldeman@users.noreply.github.com>
2021-08-03 14:12:55 -0400

    Tighten NBC check in SNH demo.
    This avoids MPI workers (where NBC=0) trying to compute the Lin vector.

--- demos/snh/snh.f90
+++ demos/snh/snh.f90
@@ -198,7 +198,7 @@
           NBC = AINT(GETP("NBC",0,U))
           IF (NBC==15) THEN
              PAR(22) = GETP("BV0",12,U) - pi
-          ELSE
+          ELSEIF (NBC==17) THEN
              ! check if Lin vector initialized:
              IF (DOT_PRODUCT(PAR(24:26),PAR(24:26)) > 0) RETURN
              DO i=1,3
