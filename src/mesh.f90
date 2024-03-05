auto-07p/auto-07p

commit 703607ec1baef53299a76f5554faccea3884da2f

Store "E" (deqf/dt) from EQDF into DEQF and use it directly in NEWMSH
This avoids an addition followed by a subtraction which resulted in
rounding errors.

src/mesh.f90
@@ -402,13 +402,13 @@ CONTAINS
     DOUBLE PRECISION, INTENT(OUT) :: TMNEW(0:NNEW)
 ! Local
     INTEGER J,J1
-    DOUBLE PRECISION X,DAL,UNEQ
-    DOUBLE PRECISION, ALLOCATABLE :: EQF(:)
-    ALLOCATE(EQF(0:NOLD))
+    DOUBLE PRECISION DAL,UNEQ
+    DOUBLE PRECISION, ALLOCATABLE :: EQF(:),DEQF(:)
+    ALLOCATE(EQF(0:NOLD),DEQF(NOLD))
 
 ! Put the values of the monotonely increasing function EQDF in EQF.
 
-    CALL EQDF(NOLD,NDIM,NCOLD,DTMOLD,UPS,NDOLD,EQF,IPER)
+    CALL EQDF(NOLD,NDIM,NCOLD,DTMOLD,UPS,NDOLD,EQF,DEQF,IPER)
 
 ! Uniformly divide the range of EQDF :
 
@@ -432,13 +432,12 @@ CONTAINS
           J=J+1
        ENDDO
        J=J-1
-       X=(UNEQ-EQF(J))/(EQF(J+1)-EQF(J))
-       TMNEW(J1)=(1.d0-X)*TMOLD(J)+X*TMOLD(J+1)
+       TMNEW(J1)=TMOLD(J)+(UNEQ-EQF(J))/DEQF(J+1)
     ENDDO
 
     TMNEW(NNEW)=TMOLD(NOLD)
 
-    DEALLOCATE(EQF)
+    DEALLOCATE(EQF,DEQF)
   END SUBROUTINE NEWMSH
 
 ! ---------- ------
@@ -471,14 +470,14 @@ CONTAINS
   END SUBROUTINE INTWTS
 
 ! ---------- ----
-  SUBROUTINE EQDF(NTST,NDIM,NCOL,DTM,UPS,NDOLD,EQF,IPER)
+  SUBROUTINE EQDF(NTST,NDIM,NCOL,DTM,UPS,NDOLD,EQF,DEQF,IPER)
 
     DOUBLE PRECISION, PARAMETER :: HMACH=1.0d-7
 
     INTEGER, INTENT(IN) :: NTST,NDIM,NCOL,NDOLD
     LOGICAL, INTENT(IN) :: IPER
     DOUBLE PRECISION, INTENT(IN) :: UPS(NDOLD,0:NTST*NCOL),DTM(NTST)
-    DOUBLE PRECISION, INTENT(OUT) :: EQF(0:NTST)
+    DOUBLE PRECISION, INTENT(OUT) :: EQF(0:NTST),DEQF(NTST)
 
 ! Local
     DOUBLE PRECISION SC,E,PWR,DTAV,ND
@@ -506,7 +505,10 @@ CONTAINS
           IF(ABS(ND)>HMACH)SMALL=.FALSE.
           HD(I)=ND
        ENDDO
-       IF(J>1)EQF(J-1)=EQF(J-2)+DTM(J-1)*E
+       IF(J>1)THEN
+          EQF(J-1)=EQF(J-2)+DTM(J-1)*E
+          DEQF(J-1)=E
+       ENDIF
     ENDDO
 
     E=0.d0
@@ -526,12 +528,15 @@ CONTAINS
        IF(ABS(ND)>HMACH)SMALL=.FALSE.
     ENDDO
     EQF(NTST)=EQF(NTST-1)+DTM(NTST)*E
+    DEQF(NTST)=E
 
 ! Take care of "small derivative" case.
 
     IF(SMALL)THEN
-       DO I=0,NTST
+       EQF(0)=0
+       DO I=1,NTST
           EQF(I)=I
+          DEQF(I)=1.0d0/DTM(I)
        ENDDO
     ENDIF
 

src/fcon.f
@@ -289,13 +289,13 @@ C Redistributes the mesh according to the function EQDF.
       DOUBLE PRECISION, INTENT(OUT) :: TMNEW(0:NNEW)
 C Local
       INTEGER J,J1
-      DOUBLE PRECISION X,DAL,UNEQ
-      DOUBLE PRECISION, ALLOCATABLE :: EQF(:)
-      ALLOCATE(EQF(0:NOLD))
+      DOUBLE PRECISION DAL,UNEQ
+      DOUBLE PRECISION, ALLOCATABLE :: EQF(:),DEQF(:)
+      ALLOCATE(EQF(0:NOLD),DEQF(1:NOLD))
 
 C Put the values of the monotonely increasing function EQDF in EQF.
 
-       CALL EQDF(NOLD,NDIM,TMOLD,UPS,NDOLD,EQF,IPER)
+       CALL EQDF(NOLD,NDIM,TMOLD,UPS,NDOLD,EQF,DEQF,IPER)
 
 C Uniformly divide the range of EQDF :
 
@@ -315,8 +315,7 @@ C value of the index of the TM-interval in which EQF(i) lies.
           J=J+1
         ENDDO
         J=J-1
-        X=(UNEQ-EQF(J))/(EQF(J+1)-EQF(J))
-        TMNEW(J1)=(1.d0-X)*TMOLD(J)+X*TMOLD(J+1)
+        TMNEW(J1)=TMOLD(J)+(UNEQ-EQF(J))/DEQF(J+1)
       ENDDO
 
 C Assign TMNEW(NNEW) explicitly because of loss of precision
@@ -324,18 +323,18 @@ C problems when EQF(NOLD) and EQF(NOLD-1) are very close
 
       TMNEW(NNEW)=TMOLD(NOLD)
 
-      DEALLOCATE(EQF)
+      DEALLOCATE(EQF,DEQF)
       END SUBROUTINE NEWMSH
 
 C     ---------- ----
-      SUBROUTINE EQDF(NTST,NDIM,TM,UPS,NDOLD,EQF,IPER)
+      SUBROUTINE EQDF(NTST,NDIM,TM,UPS,NDOLD,EQF,DEQF,IPER)
 
       DOUBLE PRECISION, PARAMETER :: HMACH=1.0d-7
 
       INTEGER, INTENT(IN) :: NTST,NDIM,NDOLD
       LOGICAL, INTENT(IN) :: IPER
       DOUBLE PRECISION, INTENT(IN) :: UPS(NDOLD,0:NTST),TM(0:NTST)
-      DOUBLE PRECISION, INTENT(OUT) :: EQF(0:NTST)
+      DOUBLE PRECISION, INTENT(OUT) :: EQF(0:NTST),DEQF(NTST)
 
 C Local
       DOUBLE PRECISION E,DTAV,ND,DT
@@ -361,7 +360,10 @@ C and define the equidistribution function :
            IF(ABS(ND)>HMACH)SMALL=.FALSE.
            HD(I)=ND
          ENDDO
-         IF(J>1)EQF(J-1)=EQF(J-2)+(TM(J-1)-TM(J-2))*E
+         IF(J>1)THEN
+            EQF(J-1)=EQF(J-2)+(TM(J-1)-TM(J-2))*E
+            DEQF(J-1)=E
+         ENDIF
        ENDDO
 
        E=0.d0
@@ -379,12 +381,15 @@ C      *else extend by extrapolation :
          IF(ABS(ND)>HMACH)SMALL=.FALSE.
        ENDDO
        EQF(NTST)=EQF(NTST-1)+(TM(NTST)-TM(NTST-1))*E
+       DEQF(NTST)=E
 
 C Take care of "small derivative" case.
 
        IF(SMALL)THEN
-         DO I=0,NTST
+         EQF(0)=0
+         DO I=1,NTST
             EQF(I)=I
+            DEQF(I)=1.0d0/(TM(I)-TM(I-1))
          ENDDO
        ENDIF
 

commit cb5dd4616be3fabd3ace9fb9f9b22fcbac9fc447

mesh: use closest point for linear time interpolation
This improves convergence in the ab demo; it had regressed a bit from
703607ec

src/mesh.f90
@@ -402,7 +402,7 @@ CONTAINS
     DOUBLE PRECISION, INTENT(OUT) :: TMNEW(0:NNEW)
 ! Local
     INTEGER J,J1
-    DOUBLE PRECISION DAL,UNEQ
+    DOUBLE PRECISION DAL,UNEQ,X1,X2
     DOUBLE PRECISION, ALLOCATABLE :: EQF(:),DEQF(:)
     ALLOCATE(EQF(0:NOLD),DEQF(NOLD))
 
@@ -432,7 +432,14 @@ CONTAINS
           J=J+1
        ENDDO
        J=J-1
-       TMNEW(J1)=TMOLD(J)+(UNEQ-EQF(J))/DEQF(J+1)
+       X1=UNEQ-EQF(J)
+       X2=EQF(J+1)-UNEQ
+       IF(X1<=X2)THEN
+          TMNEW(J1)=TMOLD(J)+X1/DEQF(J+1)
+       ELSE
+          ! More accurate FP rounding for UNEQ closer to EQF(J+1)
+          TMNEW(J1)=TMOLD(J+1)-X2/DEQF(J+1)
+       ENDIF
     ENDDO
 
     TMNEW(NNEW)=TMOLD(NOLD)